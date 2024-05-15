bb0efc3fdd30fc855843a190b2bd2f54b8ef3305
Mitul Sharma <misharma@redhat.com>
2024-05-15 11:15:31 +0530

    extract tar file

--- ktctl/cmd/root.go
+++ ktctl/cmd/root.go
@@ -1,30 +1,50 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ktctl",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "command line utility to trace kernel taint",
+	Long: `Kernel taint command line utility is a cli application to trace Error,Warning,Debug messages to troubleshoot the kernel taint issue found within requested sosreport.`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		tarfile := args[0]
+		
+		// Extract the tar.xz file
+		cmdExtract := exec.Command("tar", "-xf", tarfile)
+		cmdExtract.Stderr = os.Stderr
+		if err := cmdExtract.Run(); err != nil {
+			fmt.Println("Error extracting tar file:", err)
+			os.Exit(1)
+		}
+
+		// Wait for the extraction process to finish
+		cmdExtract.Wait()
+
+		// Get the name of the extracted directory
+		output, err := exec.Command("tar", "-tf", tarfile).Output()
+		if err != nil {
+			fmt.Println("Error getting tar file contents:", err)
+			os.Exit(1)
+		}
+
+		dir := strings.Split(string(output), "/")[0]
+
+		// Change the current directory to the extracted directory
+		if err := os.Chdir(dir); err != nil {
+			fmt.Println("Error changing directory:", err)
+			os.Exit(1)
+		}
+	 },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "ktctl/ktctl/cmd"
+
+func main() {
+	cmd.Execute()
+}
